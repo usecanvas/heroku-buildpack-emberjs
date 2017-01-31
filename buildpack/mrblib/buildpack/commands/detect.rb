3fb7af741724ab07cd7c1d431aa26c15d2565b16
Jonathan Clem <jonathan@jclem.net>
2017-01-31 16:27:52 -0500

    Add some logging

--- buildpack/mrblib/buildpack/commands/detect.rb
+++ buildpack/mrblib/buildpack/commands/detect.rb
@@ -8,12 +8,18 @@ module Buildpack::Commands
       @output_io = output_io
       @error_io  = error_io
       @build_dir = build_dir
+      @output_io.puts "Build dir: #{build_dir}"
     end
 
     def run
       package_json_path = "#{@build_dir}/package.json"
+
+      @output_io.puts "package.json path: #{package_json_path}"
+
       if File.exist?(package_json_path)
         json = JSON.parse(File.read(package_json_path))
+
+        @output_io.puts "package.json found: #{json}"
 
         if (json["devDependencies"] || {}).merge(json["dependencies"] || {})["ember-cli"]
           @output_io.puts "emberjs"
